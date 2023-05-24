ad34526fcfd60f113eabaf0a092c5ebd97ffe14b
Bryan Chan <bryanchan13@gmail.com>
2023-05-23 21:05:26 -0600

    configs for pendulum

--- docs/conf.py
+++ docs/conf.py
@@ -6,37 +6,37 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'JAX Learning'
-copyright = '2023, Bryan Chan'
-author = 'Bryan Chan'
+project = "JAX Learning"
+copyright = "2023, Bryan Chan"
+author = "Bryan Chan"
 
-version = '0.0.1'
-release = '0.0.1'
+version = "0.0.1"
+release = "0.0.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'sphinx.ext.napoleon',      # Supports Google / Numpy docstring 
-    'sphinx.ext.autodoc',       # Documentation from docstrings
-    'sphinx.ext.doctest',       # Test snippets in documentation
-    'sphinx.ext.todo',          # to-do syntax highlighting
-    'sphinx.ext.ifconfig',      # Content based configuration
+    "sphinx.ext.napoleon",  # Supports Google / Numpy docstring
+    "sphinx.ext.autodoc",  # Documentation from docstrings
+    "sphinx.ext.doctest",  # Test snippets in documentation
+    "sphinx.ext.todo",  # to-do syntax highlighting
+    "sphinx.ext.ifconfig",  # Content based configuration
 ]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "alabaster"
+html_static_path = ["_static"]
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath('..'))
+
+sys.path.insert(0, os.path.abspath("."))
+sys.path.insert(0, os.path.abspath(".."))
 source_suffix = [".rst"]
