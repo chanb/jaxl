8d5cd837bf78fe590b8bcafbbb43c104db85e14a
Bryan Chan <bryanchan13@gmail.com>
2023-05-29 13:28:35 -0600

    optional packages

--- setup.py
+++ setup.py
@@ -9,17 +9,20 @@ setup(
         "jax>=0.4.7",
         "optax>=0.1.4",
         "flax>=0.6.8",
-        "black>=23.3.0",
         "tqdm>=4.65.0",
         "orbax-checkpoint>=0.1.8",
         "matplotlib>=3.7.1",
         "gymnasium>=0.28.1",
         "tensorboard>=2.13.0",
         "torch>=2.0.0",
-        "sphinx>=7.0.1",
-        "pyment>=0.3.3",
     ],
-    extras_requires={},
+    extras_requires={
+        "formatting": [
+            "sphinx>=7.0.1",
+            "pyment>=0.3.3",
+            "black>=23.3.0",
+        ]
+    },
     packages=find_packages(include=["jaxl"]),
     include_package_data=True,
 )
