cdb6f60c0248d00e5cc99b18a6a8dd7f8f218e4e
Bryan Chan <bryanchan13@gmail.com>
2023-05-29 13:51:32 -0600

    Updated CC script

--- installation/compute_canada/initial_setup.sh
+++ installation/compute_canada/initial_setup.sh
@@ -1,15 +1,15 @@
 # Install MuJoCo binary
-cd ~
 mkdir ~/mujocodwn && cd ~/mujocodwn
 wget https://mujoco.org/download/mujoco210-linux-x86_64.tar.gz 
 mkdir ~/.mujoco
 tar -xvzf ~/mujocodwn/mujoco210-linux-x86_64.tar.gz -C ~/.mujoco/
+rm -rf ~/mujocodwn
 
 # Install Python packages
 module load python/3.9
 module load mujoco
-python -m venv jaxl_env
-source jaxl_env/bin/activate
+python -m venv ~/jaxl_env
+source ~/jaxl_env/bin/activate
 
 cd ~/scratch/jaxl
 pip install -e .
