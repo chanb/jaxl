3f91529a5a8f19ac5ca3e2f03d9d66e1b6a9bef9
Bryan Chan <bryanchan13@gmail.com>
2023-05-30 11:21:20 -0600

    BC with decision making object names

--- jaxl/learners/bc.py
+++ jaxl/learners/bc.py
@@ -1,10 +1,14 @@
-from typing import Any, Dict
+from typing import Any, Dict, Tuple
 
+import chex
+import jax
 import numpy as np
+import optax
 import timeit
 
 from jaxl.constants import *
 from jaxl.learners.supervised import SupervisedLearner
+from jaxl.utils import l2_norm
 
 
 """
@@ -17,6 +21,72 @@ class BC(SupervisedLearner):
     Behavioural Cloning (BC) algorithm. This extends `SupervisedLearner`.
     """
 
+    def _initialize_model_and_opt(self, input_dim: chex.Array, output_dim: chex.Array):
+        """
+        Construct the model and the optimizer.
+
+        :param input_dim: input dimension of the data point
+        :param output_dim: output dimension of the data point
+        :type input_dim: chex.Array
+        :type output_dim: chex.Array
+
+        """
+        super()._initialize_model_and_opt(input_dim, output_dim)
+        self._model_dict = {
+            CONST_MODEL: {CONST_POLICY: self._model_dict[CONST_MODEL]},
+            CONST_OPT_STATE: {CONST_POLICY: self._model_dict[CONST_OPT_STATE]},
+        }
+
+    def make_train_step(self):
+        """
+        Makes the training step for model update.
+        """
+
+        def _train_step(
+            model_dict: Dict[str, Any],
+            train_x: chex.Array,
+            train_carry: chex.Array,
+            train_y: chex.Array,
+            *args,
+            **kwargs,
+        ) -> Tuple[Dict[str, Any], Dict[str, Any]]:
+            """
+            The training step that computes the loss and performs model update.
+
+            :param model_dict: the model state and optimizer state
+            :param train_x: the training inputs
+            :param train_carry: the training hidden states for memory-based models
+            :param train_y: the training targets
+            :param *args:
+            :param **kwargs:
+            :type model_dict: Dict[str, Any]
+            :type train_x: chex.Array
+            :type train_carry: chex.Array
+            :type train_y: chex.Array
+            :return: the updated model state and optimizer state, and auxiliary information
+            :rtype: Tuple[Dict[str, Any], Dict[str, Any]]
+            """
+            (agg_loss, aux), grads = jax.value_and_grad(self._loss, has_aux=True)(
+                model_dict[CONST_MODEL][CONST_POLICY],
+                train_x,
+                train_carry,
+                train_y,
+            )
+            aux[CONST_AGG_LOSS] = agg_loss
+            updates, opt_state = self._optimizer.update(
+                grads,
+                model_dict[CONST_OPT_STATE][CONST_POLICY],
+                model_dict[CONST_MODEL][CONST_POLICY],
+            )
+            aux[CONST_GRAD_NORM] = {CONST_POLICY: l2_norm(grads)}
+            params = optax.apply_updates(model_dict[CONST_MODEL][CONST_POLICY], updates)
+            return {
+                CONST_MODEL: {CONST_POLICY: params},
+                CONST_OPT_STATE: {CONST_POLICY: opt_state},
+            }, aux
+
+        return _train_step
+
     def update(self, *args, **kwargs) -> Dict[str, Any]:
         """
         Updates the policy.
@@ -27,19 +97,37 @@ class BC(SupervisedLearner):
         :rtype: Dict[str, Any]
 
         """
-        tic = timeit.default_timer()
-        obss, h_states, acts_e, _, _, _, _, _, _, _ = self._buffer.sample(
-            self._config.batch_size
-        )
-        self.model_dict, aux = self.train_step(self._model_dict, obss, h_states, acts_e)
-        update_time = timeit.default_timer() - tic
-        assert np.isfinite(aux[CONST_AGG_LOSS]), f"Loss became NaN\naux: {aux}"
 
+        auxes = []
+        total_update_time = 0
+        for _ in range(self._num_updates_per_epoch):
+            tic = timeit.default_timer()
+            auxes.append({})
+            obss, h_states, acts_e, _, _, _, _, _, _, _ = self._buffer.sample(
+                self._config.batch_size
+            )
+            self.model_dict, aux = self.train_step(
+                self._model_dict, obss, h_states, acts_e
+            )
+            total_update_time += timeit.default_timer() - tic
+            assert np.isfinite(aux[CONST_AGG_LOSS]), f"Loss became NaN\naux: {aux}"
+
+            auxes[-1][CONST_AUX] = aux
+
+        auxes = jax.tree_util.tree_map(lambda *args: np.mean(args), *auxes)
         aux[CONST_LOG] = {
-            f"losses/{CONST_AGG_LOSS}": aux[CONST_AGG_LOSS],
-            f"time/{CONST_UPDATE_TIME}": update_time,
+            f"losses/{CONST_AGG_LOSS}": auxes[CONST_AUX][CONST_AGG_LOSS].item(),
+            f"time/{CONST_UPDATE_TIME}": total_update_time,
+            f"{CONST_PARAM_NORM}/pi": l2_norm(
+                self.model_dict[CONST_MODEL][CONST_POLICY]
+            ).item(),
+            f"{CONST_GRAD_NORM}/pi": auxes[CONST_AUX][CONST_GRAD_NORM][
+                CONST_POLICY
+            ].item(),
         }
         for loss_key in self._config.losses:
-            aux[CONST_LOG][f"losses/{loss_key}"] = aux[loss_key][CONST_LOSS]
+            aux[CONST_LOG][f"losses/{loss_key}"] = auxes[CONST_AUX][loss_key][
+                CONST_LOSS
+            ].item()
 
         return aux
