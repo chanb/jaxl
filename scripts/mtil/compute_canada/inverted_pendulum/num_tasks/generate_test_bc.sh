b59dc9b12f3a3126f4c1e08951972e1ccd7d58f9
Bryan Chan <bryanchan13@gmail.com>
2023-06-06 09:17:58 -0400

    Added BC script

---
+++ scripts/mtil/compute_canada/inverted_pendulum/num_tasks/generate_test_bc.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --account=def-schuurma
+#SBATCH --time=6:00:00
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=8G
+#SBATCH --array=1
+#SBATCH --output=/home/chanb/scratch/jaxl/run_reports/%j.out
+
+module load python/3.9
+module load mujoco
+source ~/jaxl_env/bin/activate
+`sed -n "${SLURM_ARRAY_TASK_ID}p" < export-generate_test_bc-gravity-num_tasks_analysis.dat`
+echo ${SLURM_ARRAY_TASK_ID}
+
+echo "Current working directory is `pwd`"
+echo "Running on hostname `hostname`"
+
+echo "Starting run at: `date`"
+python3 /home/chanb/scratch/jaxl/jaxl/main.py \
+  --config_path=${config_path} \
+  --run_seed=${run_seed}
+echo "Program test finished with exit code $? at: `date`"
