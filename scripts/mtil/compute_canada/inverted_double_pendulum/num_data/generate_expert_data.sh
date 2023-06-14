a84e6f0e7422646e6af29c75e1f90cbe064ca5f6
Bryan Chan <chanb@gra-login2.graham.sharcnet>
2023-06-14 12:07:21 -0400

    Add script to generate more data

--- scripts/mtil/compute_canada/inverted_double_pendulum/num_data/generate_expert_data.sh
+++ scripts/mtil/compute_canada/inverted_double_pendulum/num_data/generate_expert_data.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
 #SBATCH --account=def-schuurma
-#SBATCH --time=12:00:00
+#SBATCH --time=01:00:00
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=8G
-#SBATCH --array=1-7
+#SBATCH --mem=6G
+#SBATCH --array=1-3
 #SBATCH --output=/home/chanb/scratch/jaxl/run_reports/inverted_double_pendulum/%j.out
 
 module load python/3.9
 module load mujoco
 source ~/jaxl_env/bin/activate
-`sed -n "${SLURM_ARRAY_TASK_ID}p" < export-generate_train_mtbc-gravity-num_tasks_analysis.dat`
+`sed -n "${SLURM_ARRAY_TASK_ID}p" < export-generate_expert_data-gravity-num_data_analysis.dat`
 echo ${SLURM_ARRAY_TASK_ID}
 
 echo "Current working directory is `pwd`"
 echo "Running on hostname `hostname`"
 
 echo "Starting run at: `date`"
-python3 /home/chanb/scratch/jaxl/jaxl/main.py \
-  --config_path=${config_path} \
-  --run_seed=${run_seed}
+python3 /home/chanb/scratch/jaxl/jaxl/evaluate_rl_agents.py \
+  --save_buffer=${save_buffer} \
+  --run_path=${run_path} \
+  --run_seed=${run_seed} \
+  --env_seed=${env_seed} \
+  --num_episodes=${num_episodes} \
+  --buffer_size=${buffer_size}
+
 echo "Program test finished with exit code $? at: `date`"
