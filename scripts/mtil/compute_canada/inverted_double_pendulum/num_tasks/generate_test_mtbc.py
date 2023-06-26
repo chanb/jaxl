4f0a1c6ba500f9ea554fc019bde52e1f94d09c08
Bryan Chan <bryanchan13@gmail.com>
2023-06-26 09:45:16 -0600

    Updated

--- scripts/mtil/compute_canada/inverted_double_pendulum/num_tasks/generate_test_mtbc.py
+++ scripts/mtil/compute_canada/inverted_double_pendulum/num_tasks/generate_test_mtbc.py
@@ -98,7 +98,10 @@ def main(config: FlagValues):
             with open(os.path.join(root, filename), "r") as f:
                 curr_run_config = json.load(f)
                 num_tasks = len(curr_run_config["learner_config"]["buffer_configs"])
-            runs_info.append((run_path, num_tasks))
+                encoder_model_seed = curr_run_config["learner_config"]["seeds"][
+                    "model_seed"
+                ]
+            runs_info.append((run_path, num_tasks, encoder_model_seed))
 
     # Standard template
     template["logging_config"]["experiment_name"] = ""
@@ -109,7 +112,7 @@ def main(config: FlagValues):
     for idx, (model_seed, run_info) in enumerate(
         itertools.product(model_seeds, runs_info)
     ):
-        (run_path, num_tasks) = run_info
+        (run_path, num_tasks, encoder_model_seed) = run_info
         dir_i = str(idx // NUM_FILES_PER_DIRECTORY)
         curr_script_dir = os.path.join(base_script_dir, dir_i)
         curr_run_dir = os.path.join(base_run_dir, dir_i)
@@ -117,7 +120,7 @@ def main(config: FlagValues):
             os.makedirs(curr_run_dir, exist_ok=True)
             os.makedirs(curr_script_dir, exist_ok=True)
 
-        variant = f"variant-model_seed_{model_seed}-num_tasks_{num_tasks}"
+        variant = f"variant-model_seed_{model_seed}-num_tasks_{num_tasks}-encoder_model_seed_{encoder_model_seed}"
         template["learner_config"]["seeds"]["model_seed"] = int(model_seed)
         template["learner_config"]["load_encoder"] = os.path.join(
             run_path, "termination_model"
@@ -126,7 +129,9 @@ def main(config: FlagValues):
             "load_buffer"
         ] = config.test_data_path
         template["logging_config"]["save_path"] = curr_run_dir
-        template["logging_config"]["experiment_name"] = f"num_tasks_{num_tasks}"
+        template["logging_config"][
+            "experiment_name"
+        ] = f"num_tasks_{num_tasks}-encoder_model_seed_{encoder_model_seed}"
 
         out_path = os.path.join(curr_script_dir, variant)
         with open(f"{out_path}.json", "w+") as f:
