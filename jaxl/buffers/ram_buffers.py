Fix softmax policy and gaussian policy

jaxl/buffers/ram_buffers.py
@@ -68,7 +68,9 @@ class AbstractNumPyBuffer(ReplayBuffer):
             self.hidden_states = np.zeros(
                 shape=(buffer_size, *h_state_dim), dtype=np.float32
             )
-            self.actions = np.zeros(shape=(buffer_size, *act_dim[:-1]), dtype=np.float32)
+            self.actions = np.zeros(
+                shape=(buffer_size, *act_dim[:-1]), dtype=np.float32
+            )
             self.rewards = np.zeros(shape=(buffer_size, *rew_dim), dtype=np.float32)
             self.dones = np.zeros(shape=(buffer_size, 1), dtype=np.float32)
             self.terminateds = np.zeros(shape=(buffer_size, 1), dtype=np.float32)

jaxl/losses/reinforcement.py
@@ -277,7 +277,7 @@ def make_ppo_pi_loss(
         lprobs, _ = policy.lprob(params, obss, h_states, acts)
         is_ratio = jnp.exp(lprobs - old_lprobs)
         # XXX: Deal with inf values
-        is_ratio = jnp.nan_to_num(is_ratio, posinf=0.0, neginf=0.0)
+        # is_ratio = jnp.nan_to_num(is_ratio, posinf=0.0, neginf=0.0)
 
         clipped_is_ratio = jnp.clip(
             is_ratio,

jaxl/models/__init__.py
@@ -1,7 +1,8 @@
 from types import SimpleNamespace
-from typing import Any, Dict, Tuple, Union
+from typing import Any, Dict, Iterable, Tuple, Union
 
 import chex
+import numpy as np
 import optax
 
 from jaxl.constants import *
@@ -99,7 +100,7 @@ def get_model(
     ), f"{model_config.architecture} is not supported (one of {VALID_ARCHITECTURE})"
     if model_config.architecture == CONST_MLP:
         return MLP(
-            model_config.layers + list(output_dim),
+            model_config.layers + list(np.prod(output_dim, keepdims=True)),
             getattr(model_config, "activation", CONST_RELU),
         )
     elif model_config.architecture == CONST_ENCODER_PREDICTOR:
@@ -124,13 +125,15 @@ def get_model(
         raise NotImplementedError
 
 
-def get_policy(model: Model, config: SimpleNamespace) -> Policy:
+def get_policy(model: Model, act_dim: Iterable[int], config: SimpleNamespace) -> Policy:
     """
     Gets a policy
 
     :param model: a model
+    :param act_dim: the action dimensionality
     :param config: the policy configuration
     :type model: Model
+    :type act_dim: Iterable[int]
     :type config: SimpleNamespace
     :return: a policy
     :rtype: Policy
@@ -145,7 +148,9 @@ def get_policy(model: Model, config: SimpleNamespace) -> Policy:
     elif config.policy_distribution == CONST_DETERMINISTIC:
         return DeterministicPolicy(model)
     elif config.policy_distribution == CONST_SOFTMAX:
-        return SoftmaxPolicy(model)
+        return SoftmaxPolicy(
+            model, getattr(config, CONST_TEMPERATURE, DEFAULT_TEMPERATURE)
+        )
     else:
         raise NotImplementedError
 
@@ -166,5 +171,5 @@ def policy_output_dim(output_dim: chex.Array, config: SimpleNamespace) -> chex.A
         config.policy_distribution in VALID_POLICY_DISTRIBUTION
     ), f"{config.policy_distribution} is not supported (one of {VALID_POLICY_DISTRIBUTION})"
     if config.policy_distribution == CONST_GAUSSIAN:
-        return [dim * 2 for dim in output_dim]
+        return [*output_dim[:-1], output_dim[-1] * 2]
     return output_dim

jaxl/models/policies.py
@@ -1,4 +1,4 @@
-from typing import Any, Callable, Dict, Union, Tuple
+from typing import Any, Callable, Dict, Iterable, Union, Tuple
 
 import chex
 import jax
@@ -6,7 +6,13 @@ import jax.numpy as jnp
 import jax.random as jrandom
 import optax
 
-from jaxl.constants import DEFAULT_MIN_STD, CONST_MEAN, CONST_STD, CONST_LOGITS
+from jaxl.constants import (
+    DEFAULT_MIN_STD,
+    CONST_MEAN,
+    CONST_STD,
+    CONST_LOGITS,
+    DEFAULT_TEMPERATURE,
+)
 from jaxl.distributions import Normal, Softmax
 from jaxl.models.common import Model, Policy, StochasticPolicy
 
@@ -483,13 +489,15 @@ class SoftmaxPolicy(StochasticPolicy):
     def __init__(
         self,
         model: Model,
-        temperature: float = 1.0,
+        act_dim: Iterable[int],
+        temperature: float = DEFAULT_TEMPERATURE,
     ):
         super().__init__(model)
         assert temperature > 0.0, "temperature needs to be positive, got {}".format(
             temperature
         )
         self._temperature = temperature
+        self.act_dim = act_dim
         self.deterministic_action = jax.jit(self.make_deterministic_action(model))
         self.random_action = jax.jit(self.make_random_action(model))
         self.compute_action = jax.jit(self.make_compute_action(model))
@@ -542,6 +550,7 @@ class SoftmaxPolicy(StochasticPolicy):
 
             """
             act_params, h_state = model.forward(params, obs, h_state)
+            act_params = act_params.reshape(*act_params.shape[:-1], *self.act_dim)
             act = Softmax.sample(act_params / self._temperature, key)
             return act, h_state
 
@@ -585,6 +594,7 @@ class SoftmaxPolicy(StochasticPolicy):
 
             """
             act_params, h_state = model.forward(params, obs, h_state)
+            act_params = act_params.reshape(*act_params.shape[:-1], *self.act_dim)
             act_max, _ = jnp.argmax(act_params, axis=-1)
             return act_max, h_state
 
@@ -636,6 +646,7 @@ class SoftmaxPolicy(StochasticPolicy):
 
             """
             act_params, h_state = model.forward(params, obs, h_state)
+            act_params = act_params.reshape(*act_params.shape[:-1], *self.act_dim)
             act = Softmax.sample(act_params / self._temperature, key)
             return act, h_state
 
@@ -687,6 +698,7 @@ class SoftmaxPolicy(StochasticPolicy):
 
             """
             act_params, h_state = model.forward(params, obs, h_state)
+            act_params = act_params.reshape(*act_params.shape[:-1], *self.act_dim)
             act_params = act_params / self._temperature
             act = Softmax.sample(act_params, key)
             lprob = Normal.lprob(act_params, act).sum(-1, keepdims=True)
@@ -735,6 +747,7 @@ class SoftmaxPolicy(StochasticPolicy):
 
             """
             act_params, _ = model.forward(params, obs, h_state)
+            act_params = act_params.reshape(*act_params.shape[:-1], *self.act_dim)
             act_params = act_params / self._temperature
             lprob = Softmax.lprob(act_params, act).sum(-1, keepdims=True)
             return lprob, {CONST_LOGITS: act_params}