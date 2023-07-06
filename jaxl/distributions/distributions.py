009d4566ff9a3b5ed3b78b5b6f6257c57c4c62e2
Bryan Chan <bryanchan13@gmail.com>
2023-07-06 14:45:08 -0600

    Add softmax distribution

--- jaxl/distributions/distributions.py
+++ jaxl/distributions/distributions.py
@@ -3,6 +3,7 @@ from typing import Optional
 
 import chex
 import jax.numpy as jnp
+import jax.scipy as js
 import jax.random as jrandom
 import math
 
@@ -97,3 +98,50 @@ class Normal(Distribution):
         return (
             -((x - mean) ** 2) / (2 * var) - log_std - math.log(math.sqrt(2 * math.pi))
         )
+
+
+class Softmax(Distribution):
+    """
+    Softmax distribution that extends the ``Distribution`` class
+    """
+
+    @staticmethod
+    def sample(
+        logits: chex.Array,
+        key: jrandom.PRNGKey,
+        num_samples: Optional[int] = None,
+    ) -> chex.Array:
+        """
+        Samples from softmax distribution.
+
+        :param logits: logits of the softmax distribution
+        :param key: the random number generator key
+        :param num_samples: the number of samples to have
+        :type logits: chex.Array
+        :type key: jrandom.PRNGKey
+        :type num_samples: Optional[int]:  (Default value = None)
+        :return: the samples
+        :rtype: chex.Array
+
+        """
+        if num_samples:
+            shape = (num_samples, *logits.shape[:-1])
+        else:
+            shape = logits.shape[:-1]
+        return jrandom.categorical(key, logits, shape=shape)
+
+    def lprob(logits: chex.Array, x: chex.Array) -> chex.Array:
+        """
+        Computes the log probabilities with softmax distribution.
+
+        :param logits: logits of the softmax distribution
+        :param x: the samples
+        :type logits: chex.Array
+        :type x: chex.Array
+        :return: the log probabilities of the given samples
+        :rtype: chex.Array
+
+        """
+        return jnp.sum(
+            jnp.eye(logits.shape[-1])[x.astype(int)] * logits, axis=-1
+        ) - js.special.logsumexp(logits, axis=-1)
