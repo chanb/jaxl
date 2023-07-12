b1972ec42c3a02555a872980e533230b9db5c405
Bryan Chan <chanb@beluga2.int.ets1.calculquebec.ca>
2023-07-12 00:30:21 -0400

    Updated

--- scripts/mtil/compute_canada/search_expert.py
+++ scripts/mtil/compute_canada/search_expert.py
@@ -195,32 +195,34 @@ def main(config):
         dat_content += "export run_seed={} ".format(config.run_seed)
         dat_content += "config_path={}.json \n".format(out_path)
 
+    dat_path = os.path.join(f"./export-search_expert-{config.exp_name}.dat")
     with open(
-        os.path.join(f"./export-search_expert-{config.exp_name}.dat"), "w+"
+        dat_path, "w+"
     ) as f:
         f.writelines(dat_content)
 
-    os.makedirs()
-    sbatch_content = "/home/chanb/scratch/run_reports/search_expert-{}".format(config.exp_name)
-    sbatch_content += "#!/bin/bash \n"
-    sbatch_content += "#SBATCH --account=def-schuurma \n"
-    sbatch_content += "#SBATCH --time={} \n".format(config.run_time)
-    sbatch_content += "#SBATCH --cpus-per-task=1 \n"
-    sbatch_content += "#SBATCH --mem=3G \n"
-    sbatch_content += "#SBATCH --array=1:{} \n".format(num_runs)
-    sbatch_content += "#SBATCH --output=/home/chanb/scratch/run_reports/search_expert-{}/%j.out \n".format(config.exp_name)
-    sbatch_content += "module load python/3.9 \n"
-    sbatch_content += 'module load mujoco \n'
-    sbatch_content += 'source ~/jaxl_env/bin/activate \n'
-    sbatch_content += '`sed -n "${SLURM_ARRAY_TASK_ID}p" < export-generate_expert_data-gravity-num_data_analysis.dat` \n'
-    sbatch_content += 'echo ${SLURM_ARRAY_TASK_ID} \n'
-    sbatch_content += 'echo "Current working directory is `pwd`" \n'
-    sbatch_content += 'echo "Running on hostname `hostname`" \n'
-    sbatch_content += 'echo "Starting run at: `date`" \n'
-    sbatch_content += 'python3 /home/chanb/scratch/jaxl/jaxl/main.py \\ \n'
-    sbatch_content += '  --config_path=${config_path} \\ \n'
-    sbatch_content += '  --run_seed=${run_seed} \n'
-    sbatch_content += 'echo "Program test finished with exit code $? at: `date`" \n'
+    os.makedirs("/home/chanb/scratch/run_reports/search_expert-{}".format(config.exp_name), exist_ok=True)
+    sbatch_content = ""
+    sbatch_content += "#!/bin/bash\n"
+    sbatch_content += "#SBATCH --account=def-schuurma\n"
+    sbatch_content += "#SBATCH --time={}\n".format(config.run_time)
+    sbatch_content += "#SBATCH --cpus-per-task=1\n"
+    sbatch_content += "#SBATCH --mem=3G\n"
+    sbatch_content += "#SBATCH --array=1:{}\n".format(num_runs)
+    sbatch_content += "#SBATCH --output=/home/chanb/scratch/run_reports/search_expert-{}/%j.out\n".format(config.exp_name)
+    sbatch_content += "module load python/3.9\n"
+    sbatch_content += 'module load mujoco\n'
+    sbatch_content += 'source ~/jaxl_env/bin/activate\n'
+    sbatch_content += '`sed -n "${SLURM_ARRAY_TASK_ID}p"'
+    sbatch_content += " < {}`\n".format(dat_path)
+    sbatch_content += 'echo ${SLURM_ARRAY_TASK_ID}\n'
+    sbatch_content += 'echo "Current working directory is `pwd`"\n'
+    sbatch_content += 'echo "Running on hostname `hostname`"\n'
+    sbatch_content += 'echo "Starting run at: `date`"\n'
+    sbatch_content += 'python3 /home/chanb/scratch/jaxl/jaxl/main.py \\\n'
+    sbatch_content += '  --config_path=${config_path} \\\n'
+    sbatch_content += '  --run_seed=${run_seed}\n'
+    sbatch_content += 'echo "Program test finished with exit code $? at: `date`"\n'
 
     with open(os.path.join(f"./run_all-{config.exp_name}.sh"), "w+") as f:
         f.writelines(sbatch_content)
