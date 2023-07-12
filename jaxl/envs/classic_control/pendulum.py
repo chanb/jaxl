c6c8b7e6aa48b6120d49802ffaeff6cf1d7326dc
Bryan Chan <bryanchan13@gmail.com>
2023-07-11 20:32:42 -0600

    Updated action space

--- jaxl/envs/classic_control/pendulum.py
+++ jaxl/envs/classic_control/pendulum.py
@@ -135,11 +135,9 @@ class ParameterizedPendulumEnv(gym.Env):
         #   or normalised as max_torque == 2 by default. Ignoring the issue here as the default settings are too old
         #   to update to follow the gymnasium api
         if discrete_control:
-            # action_map = np.concatenate(
-            #     [([-2.0, 2.0] ** np.arange(-3, 2)[:, None]).flatten(), [0]]
-            # )
-
-            action_map = np.arange(-1, 1.25, 0.25)
+            action_map = np.concatenate(
+                [([-2.0, 2.0] ** np.arange(-3, 2)[:, None]).flatten(), [0]]
+            )
             self.action_space = spaces.Discrete(len(action_map))
 
             def process_action(u):
@@ -151,7 +149,7 @@ class ParameterizedPendulumEnv(gym.Env):
             )
 
             def process_action(u):
-                return np.clip(u, -self.max_torque, self.max_torque)[0]
+                return np.clip(u * self.max_torque, -self.max_torque, self.max_torque)[0]
 
         self.process_action = process_action
         self.observation_space = spaces.Box(low=-high, high=high, dtype=np.float32)
