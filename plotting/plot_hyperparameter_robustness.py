12485981768115f3b39ce5d627fd7e0f879669f3
Bryan Chan <bryanchan13@gmail.com>
2023-07-20 12:12:57 -0600

    Updated

--- plotting/plot_hyperparameter_robustness.py
+++ plotting/plot_hyperparameter_robustness.py
@@ -3,16 +3,17 @@ from orbax.checkpoint import PyTreeCheckpointer, CheckpointManager
 from typing import Iterable
 
 import _pickle as pickle
+import json
 import itertools
+import math
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
 import os
 
 from jaxl.constants import *
-from jaxl.envs.rollouts import EvaluationRollout
 from jaxl.utils import RunningMeanStd, flatten_dict
-from plot_utils import set_size, pgf_with_latex, get_evaluation_components
+from plot_utils import set_size, pgf_with_latex
 
 
 # Use the seborn style
@@ -32,27 +33,34 @@ hyperparameter_path = (
 save_path = f"./results-{experiment_name}"
 os.makedirs(save_path, exist_ok=True)
 
+reference_config_path = "/Users/chanb/research/personal/mtil_results/data/search_expert/cheetah/discrete/scripts/0/variant-71.json"
+top_k = 3
+smoothing = 20
 num_evaluation_episodes = 5
 env_seed = 9999
 record_video = False
 
 assert os.path.isdir(experiment_dir), f"{experiment_dir} is not a directory"
 assert os.path.isfile(hyperparameter_path), f"{hyperparameter_path} is not a file"
+assert os.path.isfile(reference_config_path), f"{reference_config_path} is not a file"
+
+with open(reference_config_path, "r") as f:
+    reference_config = json.load(f)
 
 with open(hyperparameter_path, "rb") as f:
     (hyperparam_keys, hyperparamss) = pickle.load(f)
     num_envs = len(hyperparamss[-2])
     num_models = len(hyperparamss[-1])
+    num_variants = len(list(itertools.product(*hyperparamss)))
+    num_hyperparamss = num_variants // (num_envs * num_models)
 
 if os.path.isfile(f"{save_path}/returns.pkl"):
-    (result_per_variant, env_configs) = pickle.load(
+    (result_per_variant, env_configs, match_hyperparams_i) = pickle.load(
         open(f"{save_path}/returns.pkl", "rb")
     )
 else:
     result_per_variant = {}
     env_configs = {}
-
-    num_variants = len(list(itertools.product(*hyperparamss)))
     agent_paths = {}
     for agent_path, _, filenames in os.walk(experiment_dir):
         variant_name = os.path.basename(os.path.dirname(agent_path))
@@ -64,6 +72,7 @@ else:
                 break
         agent_paths[int(variant_name.split("-")[1])] = agent_path
 
+    match_hyperparams_i = None
     for variant_i, hyperparams in enumerate(itertools.product(*hyperparamss)):
         if (variant_i + 1) % 10 == 0:
             print(f"Processed {variant_i + 1}/{num_variants} variants")
@@ -73,85 +82,121 @@ else:
         hyperparam_i = variant_i // (num_envs * num_models)
 
         variant_name = f"hyperparam_{hyperparam_i}-env_seed_{env_seed}"
-
-        result_per_variant[variant_name] = {}
         
         agent_path = agent_paths[variant_i]
+
+        if match_hyperparams_i is None:
+            match_hyperparams = True
+            curr_config_path = os.path.join(agent_path, "config.json")
+            with open(curr_config_path, "r") as f:
+                curr_config = json.load(f)
+
+                for key in ("model_config", "learner_config", "optimizer_config"):
+                    match_hyperparams = match_hyperparams and (curr_config[key] == reference_config[key])
+            if match_hyperparams:
+                match_hyperparams_i = hyperparam_i
 
         env_config_path = os.path.join(agent_path, "env_config.pkl")
         env_config = None
         if os.path.isfile(env_config_path):
             env_config = pickle.load(open(env_config_path, "rb"))
 
-        env, policy = get_evaluation_components(agent_path)
-
         checkpoint_manager = CheckpointManager(
             os.path.join(agent_path, "models"),
             PyTreeCheckpointer(),
         )
-        for checkpoint_step in checkpoint_manager.all_steps():
-            if (
-                record_video
-                and checkpoint_step == checkpoint_manager.latest_step()
-            ):
-                env = RecordVideoV0(
-                    env,
-                    f"{save_path}/videos/variant_{variant_name}-model_seed_{model_seed}/model_id_{checkpoint_step}",
-                    disable_logger=True,
-                )
-            params = checkpoint_manager.restore(checkpoint_step)
-            model_dict = params[CONST_MODEL_DICT]
-            agent_policy_params = model_dict[CONST_MODEL][CONST_POLICY]
-            agent_obs_rms = False
-            if CONST_OBS_RMS in params:
-                agent_obs_rms = RunningMeanStd()
-                agent_obs_rms.set_state(params[CONST_OBS_RMS])
 
-            agent_rollout = EvaluationRollout(env, seed=env_seed)
-            agent_rollout.rollout(
-                agent_policy_params,
-                policy,
-                agent_obs_rms,
-                num_evaluation_episodes,
-                None,
-                use_tqdm=False,
-            )
+        checkpoint = checkpoint_manager.restore(
+            checkpoint_manager.latest_step()
+        )
 
-            result_per_variant[variant_name].setdefault(checkpoint_step, [])
-            result_per_variant[variant_name][checkpoint_step].append(
-                np.mean(agent_rollout.episodic_returns)
-            )
-        env_configs[variant_name] = env_config["modified_attributes"]
+        result_per_variant.setdefault(env_seed, {})
+        result_per_variant[env_seed].setdefault(hyperparam_i, [])
+        result_per_variant[env_seed][hyperparam_i].append(checkpoint[CONST_AUX][
+            CONST_EPISODIC_RETURNS
+        ][:-1])
+        env_configs[env_seed] = env_config["modified_attributes"]
 
     with open(f"{save_path}/returns.pkl", "wb") as f:
-        pickle.dump((result_per_variant, env_configs), f)
+        pickle.dump((result_per_variant, env_configs, match_hyperparams_i), f)
 
 # Plot main return
-fig, ax = plt.subplots(1, 1, figsize=set_size(doc_width_pt, 0.49, (1, 1)))
+num_cols = 5
+num_rows = math.ceil(num_envs / num_cols)
+fig, axes = plt.subplots(
+    num_rows,
+    num_cols,
+    figsize=set_size(doc_width_pt, 0.95, (num_rows, num_cols))
+)
 
-for variant_name, returns in result_per_variant.items():
-    iteration = list(returns.keys())
-    means = []
-    stds = []
-    for val in returns.values():
-        means.append(np.mean(val))
-        stds.append(np.std(val))
-    means = np.array(means)
-    stds = np.array(stds)
+aucs_per_seed = {}
+top_k_per_seed = []
+for ax_i, (env_seed, result_per_hyperparam) in enumerate(result_per_variant.items()):
+    aucs_per_seed[env_seed] = []
+    for hyperparam_i in range(num_hyperparamss):
+        aucs_per_seed[env_seed].append(
+            np.sum(np.mean(result_per_hyperparam[hyperparam_i], axis=0))
+        )
+    aucs_per_seed[env_seed] = np.array(aucs_per_seed[env_seed])
+    top_k_idxes = np.argsort(aucs_per_seed[env_seed])[-top_k:]
 
-    sort_idxes = np.argsort(iteration)
-    iteration = np.array(iteration)
-    ax.plot(iteration[sort_idxes], means[sort_idxes], marker="x", label=variant_name)
-    ax.fill_between(
-        iteration[sort_idxes],
-        means[sort_idxes] + stds[sort_idxes],
-        means[sort_idxes] - stds[sort_idxes],
-        alpha=0.3,
+    if num_cols == num_rows == 1:
+        ax = axes
+    elif num_cols == 1 or num_rows == 1:
+        ax = axes[ax_i]
+    else:
+        ax = axes[ax_i // num_cols, ax_i % num_cols]
+    for hyperparam_i, idx in enumerate([*top_k_idxes, match_hyperparams_i]):
+        if idx is None:
+            continue
+
+        smoothed_returns = []
+        
+        for returns in result_per_hyperparam[idx]:
+            cumsum_returns = np.cumsum(returns)
+            last_t_episodic_returns = cumsum_returns - np.concatenate(
+                (np.zeros(smoothing), cumsum_returns[:-smoothing])
+            )
+            smoothed_returns.append(
+                last_t_episodic_returns / np.concatenate(
+                    (
+                        np.arange(1, smoothing + 1),
+                        np.ones(len(returns) - smoothing) * smoothing,
+                    )
+                )
+            )
+
+        smoothed_returns_mean = np.mean(smoothed_returns, axis=0)
+        smoothed_returns_std = np.std(smoothed_returns, axis=0)
+        num_episodes = np.arange(len(smoothed_returns_mean))
+
+        ax.plot(
+            num_episodes,
+            smoothed_returns_mean,
+            label=idx
+            if hyperparam_i != len(top_k_idxes)
+            else "{} - reference".format(idx),
+            linewidth=1.0,
+            alpha=0.7,
+            linestyle="-" if idx != hyperparam_i == len(top_k_idxes) else "--",
+        )
+        ax.fill_between(
+            num_episodes,
+            smoothed_returns_mean + smoothed_returns_std,
+            smoothed_returns_mean - smoothed_returns_std,
+            alpha=0.3,
+        )
+    ax.legend(
+        bbox_to_anchor=(0.0, 1.02, 1.0, 0.102),
+        loc="lower left",
+        ncols=math.ceil(top_k / 2),
+        mode="expand",
+        borderaxespad=0.0,
+        frameon=True,
     )
 
-ax.set_ylabel("Expected Return")
-ax.set_xlabel("Iterations")
-ax.legend()
+fig.supylabel("Expected Return")
+fig.supxlabel("Iterations")
 
 fig.tight_layout()
 fig.savefig(f"{save_path}/returns.pdf", format="pdf", bbox_inches="tight", dpi=600)
@@ -191,54 +236,54 @@ max_return_stds = np.array(max_return_stds)
 for attr_name, attr_vals in modified_attributes.items():
     modified_attributes[attr_name] = np.array(attr_vals)
 
-fraction = 0.9
-num_rows = 1
-num_cols = 1
-if len(modified_attributes) % 5 == 0:
-    num_cols = 5
-    num_rows = len(modified_attributes) // 5
-elif len(modified_attributes) % 4 == 0:
-    num_cols = 4
-    num_rows = len(modified_attributes) // 4
-elif len(modified_attributes) % 3 == 0:
-    num_cols = 3
-    num_rows = len(modified_attributes) // 3
-elif len(modified_attributes) % 2 == 0:
-    num_cols = 2
-    num_rows = len(modified_attributes) // 2
-else:
-    num_cols = len(modified_attributes)
-    num_rows = 1
+# fraction = 0.9
+# num_rows = 1
+# num_cols = 1
+# if len(modified_attributes) % 5 == 0:
+#     num_cols = 5
+#     num_rows = len(modified_attributes) // 5
+# elif len(modified_attributes) % 4 == 0:
+#     num_cols = 4
+#     num_rows = len(modified_attributes) // 4
+# elif len(modified_attributes) % 3 == 0:
+#     num_cols = 3
+#     num_rows = len(modified_attributes) // 3
+# elif len(modified_attributes) % 2 == 0:
+#     num_cols = 2
+#     num_rows = len(modified_attributes) // 2
+# else:
+#     num_cols = len(modified_attributes)
+#     num_rows = 1
 
-fig, axes = plt.subplots(
-    num_rows, num_cols, figsize=set_size(doc_width_pt, fraction, (num_rows, num_cols))
-)
-fig.supylabel("Expected Return")
+# fig, axes = plt.subplots(
+#     num_rows, num_cols, figsize=set_size(doc_width_pt, fraction, (num_rows, num_cols))
+# )
+# fig.supylabel("Expected Return")
 
-for attr_i, (attr_name, attr_vals) in enumerate(modified_attributes.items()):
-    sort_idxes = np.argsort(attr_vals)
+# for attr_i, (attr_name, attr_vals) in enumerate(modified_attributes.items()):
+#     sort_idxes = np.argsort(attr_vals)
 
-    if num_rows == num_cols == 1:
-        ax = axes
-    elif num_rows == 1 or num_cols == 1:
-        ax = axes[attr_i]
-    else:
-        ax = axes[attr_i // num_cols, attr_i % num_cols]
+#     if num_rows == num_cols == 1:
+#         ax = axes
+#     elif num_rows == 1 or num_cols == 1:
+#         ax = axes[attr_i]
+#     else:
+#         ax = axes[attr_i // num_cols, attr_i % num_cols]
 
-    x_vals = attr_vals[sort_idxes]
-    means = max_return_means[sort_idxes]
-    stds = max_return_stds[sort_idxes]
-    ax.plot(x_vals, means, marker="x")
-    ax.fill_between(
-        x_vals,
-        means + stds,
-        means - stds,
-        alpha=0.3,
-    )
+#     x_vals = attr_vals[sort_idxes]
+#     means = max_return_means[sort_idxes]
+#     stds = max_return_stds[sort_idxes]
+#     ax.plot(x_vals, means, marker="x")
+#     ax.fill_between(
+#         x_vals,
+#         means + stds,
+#         means - stds,
+#         alpha=0.3,
+#     )
 
-    ax.set_xlabel(f"{attr_name}")
+#     ax.set_xlabel(f"{attr_name}")
 
-fig.tight_layout()
-fig.savefig(
-    f"{save_path}/returns-variants.pdf", format="pdf", bbox_inches="tight", dpi=600
-)
+# fig.tight_layout()
+# fig.savefig(
+#     f"{save_path}/returns-variants.pdf", format="pdf", bbox_inches="tight", dpi=600
+# )
