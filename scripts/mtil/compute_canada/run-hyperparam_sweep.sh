9b66cc49c23cdde32a73314fc70f53732f4029cb
Bryan Chan <chanb@beluga1.int.ets1.calculquebec.ca>
2023-07-24 13:41:34 -0400

    name

--- scripts/mtil/compute_canada/run-hyperparam_sweep.sh
+++ scripts/mtil/compute_canada/run-hyperparam_sweep.sh
@@ -7,76 +7,76 @@ source ~/jaxl_env/bin/activate
 python sweep_rl.py \
     --main_path=${JAXL_PATH}/jaxl/main.py \
     --config_template=${JAXL_PATH}/scripts/mtil/experiments/configs/main/ppo.json \
-    --out_dir=${HOME}/scratch/data/hyperparam_search \
+    --out_dir=${HOME}/scratch/data/hyperparam_sweep \
     --run_seed=0 \
     --env_name=ParameterizedPendulum-v0 \
     --exp_name=pendulum_cont \
     --num_envs=6 \
     --num_runs=3 \
-    --hyperparam_set=hyperparam_search
+    --hyperparam_set=hyperparam_sweep
 
 python sweep_rl.py \
     --main_path=${JAXL_PATH}/jaxl/main.py \
     --config_template=${JAXL_PATH}/scripts/mtil/experiments/configs/main/ppo.json \
-    --out_dir=${HOME}/scratch/data/hyperparam_search \
+    --out_dir=${HOME}/scratch/data/hyperparam_sweep \
     --run_seed=0 \
     --env_name=ParameterizedPendulum-v0 \
     --exp_name=pendulum_disc \
     --discrete_control \
     --num_envs=6 \
     --num_runs=3 \
-    --hyperparam_set=hyperparam_search
+    --hyperparam_set=hyperparam_sweep
 
 python sweep_rl.py \
     --main_path=${JAXL_PATH}/jaxl/main.py \
     --config_template=${JAXL_PATH}/scripts/mtil/experiments/configs/main/ppo.json \
-    --out_dir=${HOME}/scratch/data/hyperparam_search \
+    --out_dir=${HOME}/scratch/data/hyperparam_sweep \
     --run_seed=0 \
     --env_name=DMCCheetah-v0 \
     --exp_name=cheetah_cont \
     --num_envs=6 \
     --num_runs=3 \
-    --hyperparam_set=hyperparam_search
+    --hyperparam_set=hyperparam_sweep
 
 python sweep_rl.py \
     --main_path=${JAXL_PATH}/jaxl/main.py \
     --config_template=${JAXL_PATH}/scripts/mtil/experiments/configs/main/ppo.json \
-    --out_dir=${HOME}/scratch/data/hyperparam_search \
+    --out_dir=${HOME}/scratch/data/hyperparam_sweep \
     --run_seed=0 \
     --env_name=DMCCheetah-v0 \
     --exp_name=cheetah_disc \
     --discrete_control \
     --num_envs=6 \
     --num_runs=3 \
-    --hyperparam_set=hyperparam_search
+    --hyperparam_set=hyperparam_sweep
 
 python sweep_rl.py \
     --main_path=${JAXL_PATH}/jaxl/main.py \
     --config_template=${JAXL_PATH}/scripts/mtil/experiments/configs/main/ppo.json \
-    --out_dir=${HOME}/scratch/data/hyperparam_search \
+    --out_dir=${HOME}/scratch/data/hyperparam_sweep \
     --run_seed=0 \
     --env_name=DMCWalker-v0 \
     --exp_name=walker_cont \
     --num_envs=6 \
     --num_runs=3 \
-    --hyperparam_set=hyperparam_search
+    --hyperparam_set=hyperparam_sweep
 
 python sweep_rl.py \
     --main_path=${JAXL_PATH}/jaxl/main.py \
     --config_template=${JAXL_PATH}/scripts/mtil/experiments/configs/main/ppo.json \
-    --out_dir=${HOME}/scratch/data/hyperparam_search \
+    --out_dir=${HOME}/scratch/data/hyperparam_sweep \
     --run_seed=0 \
     --env_name=DMCWalker-v0 \
     --exp_name=walker_disc \
     --discrete_control \
     --num_envs=6 \
     --num_runs=3 \
-    --hyperparam_set=hyperparam_search
+    --hyperparam_set=hyperparam_sweep
 
 chmod +x run_all-*.sh
-sbatch run_all-hyperparam_search-cheetah_discrete.sh
-sbatch run_all-hyperparam_search-cheetah_continuous.sh
-sbatch run_all-hyperparam_search-walker_discrete.sh
-sbatch run_all-hyperparam_search-walker_continuous.sh
-sbatch run_all-hyperparam_search-pendulum_discrete.sh
-sbatch run_all-hyperparam_search-pendulum_continuous.sh
+sbatch run_all-hyperparam_sweep-cheetah_discrete.sh
+sbatch run_all-hyperparam_sweep-cheetah_continuous.sh
+sbatch run_all-hyperparam_sweep-walker_discrete.sh
+sbatch run_all-hyperparam_sweep-walker_continuous.sh
+sbatch run_all-hyperparam_sweep-pendulum_discrete.sh
+sbatch run_all-hyperparam_sweep-pendulum_continuous.sh
