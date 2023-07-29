453400a238ba1774b710a8f6b253f4f8431a37a0
Bryan Chan <bryanchan13@gmail.com>
2023-07-29 09:28:02 -0600

    Updated script

--- scripts/mtil/compute_canada/run-generate_expert_data-main.sh
+++ scripts/mtil/compute_canada/run-generate_expert_data-main.sh
@@ -13,7 +13,7 @@ python generate_expert_data.py \
     --subsampling_lengths=1 \
     --run_time=05:00:00 \
     --max_episode_length=200 \
-    --runs_dir=${HOME}/scratch/data/experts/pendulum/continuous/runs \
+    --runs_dir=${HOME}/scratch/data/experts/pendulum_no_act_cost/continuous/runs \
     --exp_name=pendulum_cont
 
 python generate_expert_data.py \
@@ -25,7 +25,7 @@ python generate_expert_data.py \
     --subsampling_lengths=1 \
     --run_time=05:00:00 \
     --max_episode_length=200 \
-    --runs_dir=${HOME}/scratch/data/experts/pendulum/discrete/runs \
+    --runs_dir=${HOME}/scratch/data/experts/pendulum_no_act_cost/discrete/runs \
     --exp_name=pendulum_disc
 
 python generate_expert_data.py \
