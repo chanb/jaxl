ed1e55dcf15ea958754bb03a0ec8f05cd09a3e01
Bryan Chan <bryanchan13@gmail.com>
2023-08-01 09:42:48 -0600

    example

---
+++ scripts/mtil/local/policy_robustness-example/run_experiments.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+source /Users/chanb/research/personal/jaxl/.venv/bin/activate
+
+python /Users/chanb/research/personal/jaxl/jaxl/main.py --config_path=/Users/chanb/research/personal/jaxl/scripts/mtil/experiments/configs/policy_robustness-example/default_env.json
+python /Users/chanb/research/personal/jaxl/jaxl/main.py --config_path=/Users/chanb/research/personal/jaxl/scripts/mtil/experiments/configs/policy_robustness-example/extreme_env.json
