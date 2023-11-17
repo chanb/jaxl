6f31fc87005fa3df0e6c3c6a5c96b1331cf844f9
Bryan Chan <chanb@beluga2.int.ets1.calculquebec.ca>
2023-11-17 11:23:52 -0500

    new files for metaworld

--- scripts/mtil/compute_canada/rebuttal_ablation/metaworld/generate_data/run-generate_metaworld_data.sh
+++ scripts/mtil/compute_canada/rebuttal_ablation/metaworld/generate_data/run-generate_metaworld_data.sh
@@ -4,7 +4,7 @@ module load python/3.9
 module load mujoco
 source ~/jaxl_env/bin/activate
 
-python ../generate_metaworld_data.py \
+python ../../generate_metaworld_data.py \
     --main_path=${JAXL_PATH}/scripts/mtil/compute_canada/rebuttal_ablation/gather_metaworld_data.py \
     --out_dir=${HOME}/scratch/data/expert_data \
     --run_seed=0 \
