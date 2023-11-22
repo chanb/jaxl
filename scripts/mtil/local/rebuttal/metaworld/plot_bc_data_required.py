91db4ff9992cca0b73b16cb909da7d632f9e6e95
Bryan Chan <bryan.chan@ualberta.ca>
2023-11-21 21:57:25 -0700

    Updated

--- scripts/mtil/local/rebuttal/metaworld/plot_bc_data_required.py
+++ scripts/mtil/local/rebuttal/metaworld/plot_bc_data_required.py
@@ -3,28 +3,34 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 
-arch_name = "large_network"
-results_dir = "/home/bryanpu1/projects/jaxl/scripts/mtil/local/rebuttal/metaworld/eval_results/{}".format(arch_name)
+arch_name = "default_network"
+results_dir = "/home/bryanpu1/projects/jaxl/scripts/mtil/local/rebuttal/metaworld/eval_results/scrambling_10-dataset_seed_17/{}".format(arch_name)
 
 out_dir = "plots"
 os.makedirs(out_dir, exist_ok=True)
 
 max_num_data = 100000
-num_datas = []
+returns = {}
 for result in os.listdir(results_dir):
-    num_data = int(result.split("num_data_")[-1])
+    result_info = result.split("-")
+    num_data = int(result_info[0].split("num_data_")[-1])
     if num_data > max_num_data:
         continue
-    num_datas.append(num_data)
-num_datas = sorted(num_datas)
-
-returns = []
-for num_data in num_datas:
-    with open(os.path.join(results_dir, f"num_data_{num_data}/result.pkl"), "rb") as f:
+    returns.setdefault(num_data, [])
+    with open(os.path.join(results_dir, f"{result}/result.pkl"), "rb") as f:
         data = pickle.load(f)
-        returns.append(data["episodic_returns"])
+        returns[num_data].append(data["episodic_returns"])
+num_datas = sorted(list(returns.keys()))
 
-returns = np.array(returns)
+means = []
+stds = []
+for num_data in num_datas:
+    means.append(np.mean(returns[num_data]))
+    stds.append(np.std(returns[num_data]))
+means = np.array(means)
+stds = np.array(stds)
 
-plt.plot(num_datas, np.mean(returns, axis=-1))
-plt.savefig("test.png")
+plt.plot(num_datas, means)
+plt.fill_between(num_datas, means - stds, means + stds, alpha=0.3)
+
+plt.savefig("{}/test.png".format(out_dir))
