9db49e4dee6af217dabee166d50377cdc3c8efe4
Bryan Chan <bryan.chan@ualberta.ca>
2024-02-12 23:04:17 -0700

    Updated

--- jaxl/datasets/mnist.py
+++ jaxl/datasets/mnist.py
@@ -202,7 +202,7 @@ class MultitaskMNISTFineGrain(Dataset):
         sample_idxes = self._data["sample_idxes"][idx].tolist()
         inputs, labels = zip(*list(map(lambda ii: self._dataset[ii], sample_idxes)))
         inputs = np.concatenate([input[None] for input in inputs])
-        labels = np.array(labels)
+        labels = self._data["label_map"][labels]
         outputs = np.eye(self._data["num_classes"])[labels]
         return (inputs, outputs)
 
@@ -219,58 +219,73 @@ class StratifiedMultitaskMNISTFineGrain(Dataset):
         num_queries: int,
         seed: int = 0,
         random_label: bool = False,
-        save_path: str = None,
+        save_dir: str = None,
     ):
-        self._dataset = dataset
-
-        _, counts = np.unique(dataset.targets, return_counts=True)
-        self._min_num_per_class = np.min(counts)
-        self._label_to_idx = np.vstack(
-            [
-                np.where(dataset.targets == class_i)[0][: self._min_num_per_class]
-                for class_i in range(self.output_dim[0])
-            ]
+        dataset_name = "mnist_stratified_finegrain-train_{}-num_sequences_{}-num_queries_{}-random_label_{}-seed_{}.pkl".format(
+            dataset.train,
+            num_sequences,
+            num_queries,
+            random_label,
+            seed,
         )
-        self._num_queries = num_queries
-        self._random_label = random_label
+        loaded, data = maybe_load_dataset(save_dir, dataset_name)
 
-        if save_path is None or not os.path.isfile(save_path):
+        if not loaded:
+            _, counts = np.unique(dataset.targets, return_counts=True)
+            min_num_per_class = np.min(counts)
+            label_to_idx = np.vstack(
+                [
+                    np.where(dataset.targets == class_i)[0][: min_num_per_class]
+                    for class_i in range(self.output_dim[0])
+                ]
+            )
+            num_classes = 10
             (
-                self.context_idxes,
-                self.query_idxes,
-                self.swap_idxes,
-                self.label_map,
+                context_idxes,
+                query_idxes,
+                swap_idxes,
+                label_map,
             ) = self._generate_data(
                 dataset=dataset,
                 num_sequences=num_sequences,
+                num_queries=num_queries,
+                min_num_per_class=min_num_per_class,
+                num_classes=num_classes,
                 random_label=random_label,
                 seed=seed,
             )
-            if save_path is not None:
-                print("Saving to {}".format(save_path))
-                os.makedirs(os.path.dirname(save_path), exist_ok=True)
-                pickle.dump(
-                    (
-                        self.context_idxes,
-                        self.query_idxes,
-                        self.swap_idxes,
-                        self.label_map,
-                    ),
-                    open(save_path, "wb"),
-                )
-        else:
-            print("Loading from {}".format(save_path))
-            (
-                self.context_idxes,
-                self.query_idxes,
-                self.swap_idxes,
-                self.label_map,
-            ) = pickle.load(open(save_path, "rb"))
+
+            data = {
+                "context_idxes": context_idxes,
+                "query_idxes": query_idxes,
+                "swap_idxes": swap_idxes,
+                "label_map": label_map,
+                "num_sequences": num_sequences,
+                "num_queries": num_queries,
+                "random_label": random_label,
+                "train": dataset.train,
+                "input_shape": [*dataset[0][0].shape],
+                "num_classes": num_classes,
+                "min_num_per_class": min_num_per_class,
+                "label_to_idx": label_to_idx,
+                "seed": seed,
+            }
+            maybe_save_dataset(
+                data,
+                save_dir,
+                dataset_name,
+            )
+
+        self._dataset = dataset
+        self._data = data
 
     def _generate_data(
         self,
         dataset: Dataset,
         num_sequences: int,
+        num_queries: int,
+        min_num_per_class: int,
+        num_classes: int,
         random_label: bool,
         seed: int,
     ) -> Tuple[chex.Array, chex.Array, chex.Array]:
@@ -280,14 +295,14 @@ class StratifiedMultitaskMNISTFineGrain(Dataset):
         label_rng = np.random.RandomState(label_key)
 
         query_idxes = sample_rng.choice(
-            np.arange(len(dataset)), size=(num_sequences, self._num_queries)
+            np.arange(len(dataset)), size=(num_sequences, num_queries)
         )
 
         context_idxes = sample_rng.choice(
-            np.arange(self._min_num_per_class), size=(num_sequences, self.output_dim[0])
+            np.arange(min_num_per_class), size=(num_sequences, num_classes)
         )
 
-        label_map = np.tile(np.arange(self.output_dim[0]), reps=(num_sequences, 1))
+        label_map = np.tile(np.arange(num_classes), reps=(num_sequences, 1))
 
         swap_idxes = np.apply_along_axis(sample_rng.permutation, axis=1, arr=label_map)
 
@@ -300,37 +315,39 @@ class StratifiedMultitaskMNISTFineGrain(Dataset):
 
     @property
     def input_dim(self) -> chex.Array:
-        return [*self._dataset.data[0].shape]
+        return self._data["input_shape"]
 
     @property
     def output_dim(self) -> chex.Array:
-        return (10,)
+        return (self._data["num_classes"],)
 
     @property
     def num_queries(self) -> int:
-        return self._num_queries
+        return self._data["num_queries"]
 
     def __len__(self):
-        return len(self.query_idxes)
+        return self._data["num_sequences"]
 
     def __getitem__(self, idx):
-        context_idxes = self.context_idxes[idx]
+        context_idxes = self._data["context_idxes"][idx]
         context_idxes = np.take_along_axis(
-            self._label_to_idx, context_idxes[:, None], axis=1
+            self._data["label_to_idx"], context_idxes[:, None], axis=1
         ).flatten()
-        context_inputs = self._dataset.transform(self._dataset.data[context_idxes])
-        context_outputs = self.label_map[idx][self._dataset.targets[context_idxes]]
+        context_inputs, context_outputs = zip(*list(map(lambda ii: self._dataset[ii], context_idxes)))
+        context_inputs = np.concatenate([input[None] for context_input in context_inputs])
+        context_outputs = self._data["label_map"][context_outputs]
 
-        context_inputs = context_inputs[self.swap_idxes[idx]]
-        context_outputs = context_outputs[self.swap_idxes[idx]]
-        context_outputs = np.eye(self.output_dim[0])[context_outputs]
+        context_inputs = context_inputs[self._data["swap_idxes"][idx]]
+        context_outputs = context_outputs[self._data["swap_idxes"][idx]]
+        context_outputs = np.eye(self._data["num_classes"])[context_outputs]
 
-        query_idxes = self.query_idxes[idx].tolist()
-        queries = self._dataset.transform(self._dataset.data[query_idxes])
-        labels = np.atleast_1d(self.label_map[idx][self._dataset.targets[query_idxes]])
-        labels = np.eye(self.output_dim[0])[labels]
+        query_idxes = self._data["query_idxes"][idx].tolist()
+        queries, labels = zip(*list(map(lambda ii: self._dataset[ii], query_idxes)))
+        queries = np.concatenate([input[None] for query in queries])
+        labels = self._data["label_map"][labels]
+        outputs = np.eye(self._data["num_classes"])[labels]
 
-        return (context_inputs, context_outputs, queries, labels)
+        return (context_inputs, context_outputs, queries, outputs)
 
 
 class MultitaskMNISTRandomBinary(Dataset):
