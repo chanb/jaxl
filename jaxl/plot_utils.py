9c6585b988966c9cdf33a5f7faca25696feda7bf
Bryan Chan <bryan.chan@ualberta.ca>
2024-02-28 10:57:57 -0700

    update

--- jaxl/plot_utils.py
+++ jaxl/plot_utils.py
@@ -206,16 +206,17 @@ def icl_image_grid(
             axes[row_i, col_i].set_title(
                 "{}".format(np.argmax(context_outputs_i[context_i]))
             )
-            axes[row_i, col_i].imshow(context_inputs_i[context_i], cmap=plt.cm.binary)
-        axes[sample_i * 2, -1].imshow(query_i[0])
+            axes[row_i, col_i].imshow(np.transpose(context_inputs_i[context_i], (1, 2, 0)), cmap=plt.cm.binary)
+        axes[sample_i * 2, -1].imshow(np.transpose(query_i[0], (1, 2, 0)))
         axes[sample_i * 2, -1].set_title("Label: {}".format(np.argmax(label_i)))
         axes[sample_i * 2, -1].set_xticks([])
         axes[sample_i * 2, -1].set_yticks([])
         axes[sample_i * 2 + 1, -1].axis("off")
         axes[sample_i * 2 + 1, -1].set_title("Pred: {}".format(np.argmax(pred_i)))
         title = ""
-        for logit in pred_i:
-            title += "{:.4f}\n".format(logit)
+        top_10 = np.argmax(pred_i)[-10:]
+        for idx in top_10:
+            title += "idx {}: {:.4f}\n".format(idx, pred_i[idx])
         axes[sample_i * 2 + 1, -1].text(0.25, 0.0, title, fontsize=6)
 
     if filename is not None:
