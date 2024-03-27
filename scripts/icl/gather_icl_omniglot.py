d9141cde070a5a128137d46de46e09d459e49fd2
Bryan Chan <bryan.chan@ualberta.ca>
2024-03-26 22:35:17 -0600

    Updated code

--- scripts/icl/gather_icl_omniglot.py
+++ scripts/icl/gather_icl_omniglot.py
@@ -28,7 +28,9 @@ def get_eval_datasets(
     num_workers: int,
 ):
     pretrain_dataset_config = config_dict["learner_config"]["dataset_config"]
-    if pretrain_dataset_config["dataset_kwargs"]["task_name"].endswith("all_split"):
+    if pretrain_dataset_config["dataset_kwargs"]["task_name"].endswith(
+        "all_split"
+    ) or pretrain_dataset_config["dataset_kwargs"]["task_name"].endswith("tf"):
         n_shot_k_way_dataset_name = CONST_MULTITASK_OMNIGLOT_N_SHOT_K_WAY_ALL_SPLIT
     else:
         n_shot_k_way_dataset_name = CONST_MULTITASK_OMNIGLOT_N_SHOT_K_WAY
