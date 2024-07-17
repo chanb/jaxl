dacd31d28871159f03bde635361c4ea0388d6166
Bryan Chan <bryanchan13@gmail.com>
2024-07-17 15:29:57 -0600

    scripts and visualizationt ool

---
+++ experiments/stream_block_biuniform/ablation/high_prob.sh
@@ -0,0 +1,5 @@
+source /Users/chanb/research/personal/jaxl/.venv/bin/activate
+
+python /Users/chanb/research/personal/jaxl/main.py --config_path=/Users/chanb/research/personal/jaxl/experiments/stream_block_biuniform/ablation/high_prob/high_prob_0.5.json
+python /Users/chanb/research/personal/jaxl/main.py --config_path=/Users/chanb/research/personal/jaxl/experiments/stream_block_biuniform/ablation/high_prob/high_prob_0.75.json
+python /Users/chanb/research/personal/jaxl/main.py --config_path=/Users/chanb/research/personal/jaxl/experiments/stream_block_biuniform/ablation/high_prob/high_prob_0.99.json
---
+++ experiments/stream_block_biuniform/ablation/run_all.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+chmod +x /Users/chanb/research/personal/jaxl/experiments/stream_block_biuniform/ablation/high_prob.sh
+/Users/chanb/research/personal/jaxl/experiments/stream_block_biuniform/ablation/high_prob.sh
