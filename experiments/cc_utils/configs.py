a2f9ea213d2167af1422874e308c372f90e056ac
Bryan Chan <chanb@beluga3.int.ets1.calculquebec.ca>
2024-08-04 14:59:20 -0400

    more samples

--- experiments/cc_utils/configs.py
+++ experiments/cc_utils/configs.py
@@ -1,6 +1,12 @@
 EXPERIMENTS = {
     "random_high_prob-linearly_separable": {
-        "run_time": "01:00:00",
+        "run_time": "02:00:00",
+        "num_seeds": 10,
+        "variant": "high_prob",
+        "values": [0.5, 0.75, 0.9, 0.99, 0.999],
+    },
+    "random_high_prob-linearly_separable-more_samples": {
+        "run_time": "04:00:00",
         "num_seeds": 10,
         "variant": "high_prob",
         "values": [0.5, 0.75, 0.9, 0.99, 0.999],
