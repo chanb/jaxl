895cd00bb12cd77558485b4ccfd9f56f30318105
Bryan Chan <chanb@beluga2.int.ets1.calculquebec.ca>
2024-08-06 20:43:03 -0400

    Add more experiments

--- experiments/cc_utils/configs.py
+++ experiments/cc_utils/configs.py
@@ -22,7 +22,13 @@ EXPERIMENTS = {
         "num_seeds": 10,
         "variant": "high_prob",
         "values": [0.5, 0.75, 0.9, 0.99, 0.999],
-    }
+    },
+    "random_high_prob-linearly_separable-less_epochs": {
+        "run_time": "00:45:00",
+        "num_seeds": 10,
+        "variant": "high_prob",
+        "values": [0.5, 0.75, 0.9, 0.99, 0.999],
+    },
 }
 
 EVALUATION = {
